227091c04c8017e02c3d663ca5fbb6a8b1953e15
han-winchester <36017178+han-winchester@users.noreply.github.com>
2021-09-21 15:16:04 -0400

    Update Mouse Aiming

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,27 +8,12 @@ public class PlayerController : MonoBehaviour
     float verticalInput;
     float walkSpeed = 2f;
     float runSpeed = 4f;
-    new Transform camera;
-    public float sensitivity = 350f;
-
-
-    float headRotation = 0f;
-    float headRotationLimit = 90f;
-
 
 
     // Start is called before the first frame update
     void Start()
     {
-        camera = Camera.main.transform;
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
-    }
 
-    // Update is called once per frame
-    private void Update()
-    {
-        mouseMovement();
     }
 
     // Update is called once per frame
@@ -45,17 +30,6 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    void mouseMovement()
-    {
-        float x = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
-        float y = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime * -1f;
-
-
-        transform.localEulerAngles += new Vector3(0f, x, 0f);
-        headRotation += y;
-        headRotation = Mathf.Clamp(headRotation, -headRotationLimit, headRotationLimit);
-        camera.localEulerAngles = new Vector3(headRotation, 0f, 0f);
-    }
 
     void moveCharacter(float currentSpeed)
     {
