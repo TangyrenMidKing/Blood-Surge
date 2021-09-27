5c0fee4a60c6798f663e7db8773edd35f7929f16
han-winchester <36017178+han-winchester@users.noreply.github.com>
2021-09-27 19:14:53 -0400

    Add Speed Boost

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,29 +8,34 @@ public class PlayerController : MonoBehaviour
     float verticalInput;
     float walkSpeed = 2f;
     float runSpeed = 4f;
+    float boostWalkSpeed = 4f;
+    float boostRunSpeed = 6f;
+    bool hasBoostSpeed;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-
+        hasBoostSpeed = false;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        // Check if user is holding the left shift key
+        // Applies appropiate speed to movement depending if character is holding left shift and if character has the boost perk
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            moveCharacter(runSpeed);
+            moveCharacter(hasBoostSpeed ? boostRunSpeed : runSpeed);
         }
         else
         {
-            moveCharacter(walkSpeed);
+            moveCharacter(hasBoostSpeed ? boostWalkSpeed : walkSpeed);
         }
     }
 
 
+    // moves character base on input, takes in speed as param
     void moveCharacter(float currentSpeed)
     {
         // Lets the character body move left and right
@@ -41,4 +46,26 @@ public class PlayerController : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * currentSpeed);
     }
+
+    // Coroutine used as a countdown timer. Applies speed boost to character for 7 seconds
+    IEnumerator BoostSpeedCountDownRoutine()
+    {
+        yield return new WaitForSeconds(7f);
+
+        hasBoostSpeed = false;
+
+    }
+
+    // If player runs into the speed boost perk then destroy the speed boost object and start coroutine for countdown
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("SpeedBoost"))
+        {
+            hasBoostSpeed = true;
+            Destroy(other.gameObject);
+            StartCoroutine(BoostSpeedCountDownRoutine());
+        }
+    }
+
+
 }
