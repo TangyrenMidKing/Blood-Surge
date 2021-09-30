1ca36900458af9ddd6e9d90e048c7c8ee36ff5cb
han-winchester <36017178+han-winchester@users.noreply.github.com>
2021-09-29 22:28:55 -0400

    added health UI

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public PlayerHealth playerHealth;
+    public Text healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Access players current health from PlayerHealth class, then converts it to a string in order to display on UI
+        healthBar.text = "Health: " + playerHealth.GetComponent<PlayerHealth>().getHealth().ToString();
+    }
+}
