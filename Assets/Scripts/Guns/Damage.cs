c2d4200d4471a581763e2458ec11df375e762a77
han-winchester <36017178+han-winchester@users.noreply.github.com>
2021-11-27 13:49:47 -0500

    add firerate and different bullet damage

--- Assets/Scripts/Guns/Damage.cs
+++ Assets/Scripts/Guns/Damage.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 public class Damage : MonoBehaviour
 {
     public AudioClip impact;
+    public ShootGun shootGun;
+    public GrabWeapon grabWeapon;
     AudioSource bulletAudio;
     float audioClipLength;
-    public int damage = 30;
-    // Start is called before the first frame update
+    public int damage;
+    int currentWeapon;
+    
 
+    enum Weapons
+    {
+        M4,
+        Skorpion,
+        Ump,
+    }
+
+    // Start is called before the first frame update
     void Start()
     {
         bulletAudio = GetComponent<AudioSource>();
@@ -19,6 +30,7 @@ public class Damage : MonoBehaviour
 
     void OnCollisionEnter(Collision other)
     {
+        
         Target target = other.gameObject.GetComponent<Target>();
         if (target != null)
         {
@@ -45,7 +57,28 @@ public class Damage : MonoBehaviour
     // // Update is called once per frame
     void Update()
     {
-        
+
+        // check current weapon to determine how much damage
+        currentWeapon = grabWeapon.GetComponent<GrabWeapon>().getCurrentWeapon(); // gets the index of the weapon in the weaponList array
+
+        switch (currentWeapon)
+        {
+            case (int)Weapons.M4:
+                damage = 30;
+                break;
+
+            case (int)Weapons.Skorpion:
+                damage = 20;
+                break;
+
+            case (int)Weapons.Ump:
+                damage = 70;
+                break;
+
+            default:
+                damage = 100;
+                break;
+        }
     }
 
 }
